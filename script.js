f10b3ba151d59ebb510bb0fd32e6c81b0380c3d1
mohammedfaiz17052012-cell <mohammed.faiz17052012@gmail.com>
2025-10-06 14:26:34 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Form confirmation
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting Vertex Luis. We will reply soon!");
+});
